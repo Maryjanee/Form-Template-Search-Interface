d608c462f69bf03fa81c9fd5d6235fa31ec97579
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-03 14:47:41 +0100

    Add searchReducer

---
+++ src/reducers/search.js
@@ -0,0 +1,16 @@
+import { act } from "react-dom/test-utils";
+import { SEARCH_TEMPLATES_BY_CATEGORIES } from "../constants";
+
+const intialState = {searchQuery: ""}
+
+
+
+export const searchReducer = (state= intialState, action)=> {
+   switch (action.type){
+     case SEARCH_TEMPLATES_BY_CATEGORIES:
+       return {...state, searchQuery: action.payload}
+       default:
+         return state;
+   }
+
+}
