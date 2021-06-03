b4ac9e2666716b5415b48e1d400176b6b1de39ce
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-03 14:47:10 +0100

    Add templateReducer

---
+++ src/reducers/templates.js
@@ -0,0 +1,24 @@
+import {GET_TEMPLATES_REQUEST, 
+  GET_TEMPLATES_FULFILLED,
+  GET_TEMPLATES_ERROR} from '../constants';
+
+const initialState = {
+  pending:false,
+  templates:[],
+  error:null
+}
+
+export const templatesReducer = (state = initialState, action) =>{
+  switch(action.type){
+    case GET_TEMPLATES_REQUEST :
+      return {...state, pending:true}
+
+    case GET_TEMPLATES_FULFILLED :
+      return {...state, templates:action.payload, error:""}
+      
+    case GET_TEMPLATES_ERROR :
+      return {...state, error:action.payload}
+      default:
+      return state;
+  }
+}
