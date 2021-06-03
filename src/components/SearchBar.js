595f67f1cab95c9685b62db402c5e7a863245e46
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-03 11:18:10 +0100

    Add Search Component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import '../styles/SearchBar.css';
+const SearchBar = () => {
+  return (
+    <div className="search-bar-container">
+        <input type="text" 
+        className='search-templates'
+        placeholder='Search Templates'
+        />
+    </div>
+  )
+}
+
+export default SearchBar;
