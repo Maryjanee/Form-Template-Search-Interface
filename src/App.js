4dce75693bc859485f370f38cbd4ac70f3f69529
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-06-05 06:15:39 +0100

    Update App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from "./components/SearchBar";
+import SortBy from "./components/SortBy";
+import Templates from "./container/Templates";
+import "../src/styles/App.scss";
+import important from "../src/assets/important.svg";
 
-function App() {
+
+const App = () => {
   return (
-    <div>
-      Form Template Search Interface
+    <div className="container">
+      <header className="d-flex">
+        <SearchBar />
+        <SortBy />
+      </header>
+      <div className="alert d-flex">
+        <div className="icon-container">
+          <img className="icon" src={important} alt="important-icon" />
+        </div>
+        <p>
+          Tada! Get started with a free template. Can't find what you were
+          looking for?Search from 1000+ available templates
+        </p>
+      </div>
+
+      <Templates />
     </div>
   );
-}
+};
 
 export default App;
